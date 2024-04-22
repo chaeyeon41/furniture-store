1a2213463f4e0fa0a10cfa5b8ecd7920cbffc54a
chaeyeon41 <rtfgvb098@naver.com>
2024-04-22 10:26:45 +0900

    등록 페이지 커밋

--- src/store/Furniture.jsx
+++ src/store/Furniture.jsx
@@ -17,8 +17,8 @@ export const frntList = [
         frnt_title: "책장",
         frnt_info: "집 안의 마법 같은 공간. 우리의 책장으로 당신의 공간을 더 아름답고 기능적으로 만들어보세요. 서적을 보관하고 전시할 수 있는 편리함과 아름다운 디자인을 경험하세요. 당신의 책 소중한 공간을 위해 최상의 솔루션을 제공합니다.",
         frnt_price: "35,200",
-        frnt_address: "경북 구미시 인동36길 23-34 (구평동, 7단지부영아파트)",
-        frnt_detail_Address: "000동 000호",
+        frnt_address: "경북 구미시 산호대로29길 7-17",
+        frnt_detail_Address: "다이소 구미옥계점",
         frnt_favorite_count: 11,
         frnt_img: '/img/책장.jpg',
     },
@@ -26,11 +26,38 @@ export const frntList = [
         frnt_id: 2,
         frnt_seller: '홍길동',
         frnt_title: "의자",
-        frnt_info: "당신의 편안한 공간을 위한 완벽한 의자를 찾으십니까? 우리 쇼핑몰에서는 튼튼하고 스타일리시한 의자가 여러분을 기다리고 있습니다.",
-        frnt_price: "35,200",
-        frnt_address: "경북 구미시 인동36길 23-34 (구평동, 7단지부영아파트)",
-        frnt_detail_Address: "000동 000호",
+        frnt_info: "당신의 편안한 공간을 위한 완벽한 의자를 찾으신다면 이 의자를 추천드립니다!",
+        frnt_price: "19,200",
+        frnt_address: "경북 구미시 산호대로29길 7-17",
+        frnt_detail_Address: "다이소 구미옥계점",
         frnt_favorite_count: 21,
         frnt_img: '/img/의자.jpg',
+    },
+]
+
+export const purchaseHistoryInfo = [
+    {
+        frnt_id: 2,
+        frnt_seller: '홍길동',
+        frnt_title: "의자",
+        frnt_price: "35,200",
+        frnt_address: "경북 구미시 산호대로29길 7-17",
+        frnt_detail_Address: "다이소 구미옥계점",
+        frnt_img: '/img/책장.jpg',
+        frnt_date: '2024-04-05',
+    },
+]
+
+
+export const salesHistoryInfo = [
+    {
+        frnt_id: 1,
+        frnt_buyer: '홍길동',
+        frnt_title: "책장",
+        frnt_price: "35,200",
+        frnt_address: "경북 구미시 산호대로29길 7-17",
+        frnt_detail_Address: "다이소 구미옥계점",
+        frnt_favorite_count: 11,
+        frnt_img: '/img/책장.jpg',
     },
 ]
